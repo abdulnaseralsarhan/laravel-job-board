03556c7ed66f520a857fdca55b7ea78817f222aa
ABDULNASERALSARHAN <abdulnaseralsarhan0@gmail.com>
2025-11-01 17:25:50 +0300

    Blade templatihg engine module

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+<x-layout :title="$pageTitle">
+  <h2>contact page</h2>
+</x-layout>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+  <h2>welcome to my page</h2>
+  <ul>
+    <li>jobs of today</li>
+    <li>jobs of weekday</li>
+  </ul>
+</x-layout>
